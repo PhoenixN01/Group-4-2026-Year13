cb0dcfc8b9ee17c61dc60174e9d48ff08e38661e
Phoenix N <pn22178@my.westlake.school.nz>
2026-03-22 23:47:39 +1300

    Implemented OSRM fetch to obtain drawn out route from lat lon coords and convert to a layer for maplibre-gl to project above the street map

--- trialing/map/trial-map-3/main.js
+++ trialing/map/trial-map-3/main.js
@@ -12,11 +12,12 @@ const map = new maplibregl.Map({
 
 map.addControl(new maplibregl.NavigationControl());
 
-map.on('load', () => {
+map.on('load', async () => {
     map.addLayer({
         id: '3d-buildings',
         source: 'openmaptiles',
         'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
         type: 'fill-extrusion',
         minzoom: 12,
         paint: {
@@ -25,6 +26,38 @@ map.on('load', () => {
             'fill-extrusion-color': '#aaa',
             'fill-extrusion-opacity': 1
         }
+    });
+
+    const routeCoords = [
+        [172.7975, -34.5046],
+        [174.7767, -41.2784]
+    ];
+
+    const coordString = routeCoords.map(c => `${c[0]},${c[1]}`).join(';');
+    const url = `https://router.project-osrm.org/route/v1/driving/${coordString}?overview=full&geometries=geojson`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    const geometry = data.routes[0].geometry;
+
+    map.addSource('route', {
+        type: 'geojson',
+        data: { type: 'Feature', geometry }
+    });
+
+    map.addLayer({
+        id: 'route-line',
+        type: 'line',
+        source: 'route',
+        paint: { 
+            'line-color': '#007AFF', 
+            'line-width': 4
+        }
+    });
+
+    routeCoords.forEach((coord, i) => {
+        const color = i === 0 ? 'green' : 'red';
+        new maplibregl.Marker({ color }).setLngLat(coord).addTo(map);
     });
 });
 
